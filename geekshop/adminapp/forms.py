415b0d9f6d286f9de73441ca4a97fb179e71591b
Cyclonus931 <Cyclonus931@gmail.com>
2021-11-11 18:17:52 +0200

    lesson-2.2-HW

---
+++ geekshop/adminapp/forms.py
@@ -0,0 +1,40 @@
+from authapp.models import ShopUser
+from authapp.forms import ShopUserEditForm
+from mainapp.models import ProductCategory
+from django import forms
+
+
+class ShopUserAdminEditForm(ShopUserEditForm):
+    class Meta:
+        model = ShopUser
+        fields = '__all__'
+
+
+class ProductCategoryEditForm(forms.ModelForm):
+    class Meta:
+        model = ProductCategory
+        fields = '__all__'
+
+
+from authapp.models import ShopUser
+from authapp.forms import ShopUserEditForm
+from django import forms
+
+from mainapp.models import Product
+
+
+class ShopUserAdminEditForm(ShopUserEditForm):
+    class Meta:
+        model = ShopUser
+        fields = '__all__'
+
+
+class ProductEditForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(ProductEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
